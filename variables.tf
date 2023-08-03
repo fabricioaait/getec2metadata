4123a5e31ae233b9ff60044ecfa42e658c16fb7c
fabricio <119947341+fabricioaait@users.noreply.github.com>
2023-08-03 21:21:47 +0100

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "instance_id" {
+  description = "The instance ID of the EC2 instance."
+}
+
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket to store instance metadata."
+}
