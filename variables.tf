c568ee38bc6a08f53bf6ca0017b64873c84e6281
fabricio <119947341+fabricioaait@users.noreply.github.com>
2023-08-05 12:28:24 +0100

    Delete variables.tf

--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "instance_id" {
-  description = "The instance ID of the EC2 instance."
-}
-
-variable "s3_bucket_name" {
-  description = "The name of the S3 bucket to store instance metadata."
-}
